53295f69a0645d7a76c52b4cf9fe66f92b928db1
Igor Sokolov <igor.sokolov@gmail.com>
2012-01-25 15:20:44 +0200

    Debug info

--- lib/oauth/rack/oauth_filter.rb
+++ lib/oauth/rack/oauth_filter.rb
@@ -46,11 +46,11 @@ module OAuth
                 oauth_token.provided_oauth_verifier = request_proxy.oauth_verifier
               end
               env["oauth.token_candidate"] = oauth_token
-              puts "!! oauth_token : #{oauth_token.inspect} "
             end
             # return the token secret and the consumer secret
             [(oauth_token.nil? ? nil : oauth_token.secret), (client_application.nil? ? nil : client_application.secret)]
         end
+        puts "!! oauth_token : #{env["oauth.token_candidate"].inspect} "
           if env["oauth.token_candidate"]
             env["oauth.token"] = env["oauth.token_candidate"]
             strategies << :oauth10_token
